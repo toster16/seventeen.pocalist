473ca9cc0c6858629e3768e4b9590c8cb5a3a253
toster16 <seorabbit1306@gmail.com>
2026-02-22 22:57:23 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE_VERSION = 'svt-poca-v1';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
